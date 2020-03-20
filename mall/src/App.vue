f7fcb2feeec0d84366dddc1a15bf58b242db52c5
liweixiong <996797360@qq.com>
2020-03-20 21:49:01 +0800

    项目目录结构设置

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+//import axios from 'axios'
+import jsonp from 'jsonp'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   },
   data(){
-    return{
-      age:30
-    }
+    return{   
+  }
   }
 }
 </script>
